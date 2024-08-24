0c1c4b72200218c676acc9bd904711a6c983423d
M-Vadim-dev <railer@bk.ru>
2024-08-25 00:16:14 +0300

    KTB-04-fix-split

--- src/main/kotlin/KTB-04.kt
+++ src/main/kotlin/KTB-04.kt
@@ -3,31 +3,26 @@ package org.example
 import java.io.File
 
 fun main() {
-    val wordsFile = File("words.csv")
+    val wordsFile = File("words.txt")
     wordsFile.createNewFile()
-    wordsFile.writeText("hello,привет|3\n")
-    wordsFile.appendText("dog,собака|1\n")
-    wordsFile.appendText("cat,кошка|2\n")
+    wordsFile.writeText("hello|привет|3\n")
+    wordsFile.appendText("dog|собака|1\n")
+    wordsFile.appendText("cat|кошка|2\n")
 
     val dictionary = mutableListOf<Word>()
 
     for (line in wordsFile.readLines()) {
         val parts = line.split("|")
-        if (parts.size == 2) {
-            val wordParts = parts[0].split(",")
-            if (wordParts.size == 2) {
-                val correctAnswersCount = parts[1].toIntOrNull() ?: 0
-                val word = Word(
-                    original = wordParts[0],
-                    translate = wordParts[1],
-                    correctAnswersCount = correctAnswersCount,
-                )
-                dictionary.add(word)
-            }
+        if (parts.size == 3) {
+            val correctAnswersCount = parts[2].toIntOrNull() ?: 0
+            val word = Word(
+                original = parts[0],
+                translate = parts[1],
+                correctAnswersCount = correctAnswersCount,
+            )
+            dictionary.add(word)
         }
     }
-
-    File("learnedWordsCount.txt").writeText("Количество выученных слов: ${dictionary.size}")
 
     dictionary.forEach { word ->
         println("Слово: ${word.original}, Перевод: ${word.translate}, Количество правильных ответов: ${word.correctAnswersCount}")
