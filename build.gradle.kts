bfcac92d28f96d0ff9e6c23250e562cfca6a8f6e
M-Vadim-dev <railer@bk.ru>
2024-10-11 00:19:10 +0300

    KTB-23

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.0.20"
     kotlin("plugin.serialization") version "2.0.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    application
 }
 
 group = "org.example"
@@ -13,4 +15,8 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+application {
+    mainClass.set("TelegramKt")
 }
